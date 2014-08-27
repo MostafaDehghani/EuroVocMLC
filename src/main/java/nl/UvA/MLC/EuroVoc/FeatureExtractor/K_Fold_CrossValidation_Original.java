a42c1eebed214ec804990f96fc819723ccf370a3
MostafaDehghani <dehghani.mostafa@gmail.com>
2014-08-27 16:08:31 +0200

    propagation and cross validation

--- src/main/java/nl/UvA/MLC/EuroVoc/FeatureExtractor/K_Fold_CrossValidation_Original.java
+++ src/main/java/nl/UvA/MLC/EuroVoc/FeatureExtractor/K_Fold_CrossValidation_Original.java
@@ -6,13 +6,10 @@
 
 package nl.UvA.MLC.EuroVoc.FeatureExtractor;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,10 +23,11 @@ import org.apache.commons.io.FileUtils;
  *
  * @author mosi
  */
-public class K_Fold_CrossValidation {
+public class K_Fold_CrossValidation_Original {
     
     private int k = Integer.parseInt(configFile.getProperty("K_IN_K-FOLD_CV"));
-    static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(K_Fold_CrossValidation.class.getName());
+    static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(K_Fold_CrossValidation_Original.class.getName());
+    
     public void crossValidation(HashMap<Integer,HashMap<EuroVocDoc,HashMap<String,Feature>>> features, String outDir, String inDir){
         try {
             File out = new File(outDir);
@@ -37,6 +35,8 @@ public class K_Fold_CrossValidation {
             File in = new File(inDir);
             File[] inFiles = in.listFiles();
             Arrays.sort(inFiles);
+            int inNumFiles = inFiles.length;
+            int numFilesPerFold = inNumFiles / k;
             Map<String, EuroVocDoc> docs = new HashMap<>();
             for(Entry<EuroVocDoc, HashMap<String, Feature>> ent : features.get(1).entrySet())
                 docs.put(ent.getKey().getId(), ent.getKey());
@@ -54,50 +54,45 @@ public class K_Fold_CrossValidation {
                 FileWriter fw = new FileWriter(file);
                 BufferedWriter bw = new BufferedWriter(fw);
                 
-                BufferedReader br = new BufferedReader(new FileReader(new File("/home/hazarbo1/data/K_Fold_DOCSNAMES/" + "fold" + i + "/testDocs.txt")));
-                ArrayList<String> list2 = new ArrayList<>();
                 int qid = 0;
-                String s;
-                while((s = br.readLine()) != null)
-                    {
-                        list2.add(s);
-                        {
-                        String docID = s.split("\\.")[0];
+                for(int j = i * numFilesPerFold; j < (i + 1) * numFilesPerFold; j++)
+                {
+                    if(inFiles[j].getName().startsWith("jrc")&& inFiles[j].getName().endsWith(".xml")){
+                        String docID = inFiles[j].getName().split("\\.")[0];
                         Map<String, Feature> firstFeature = features.get(1).get(docs.get(docID));
                         for(Entry<String, Feature> ent : firstFeature.entrySet())
                         {
                             String line = " " + "qid:" + qid + " " + "1:" + ent.getValue().getfValue() + " ";
-                    
                             for(int l = 1; l < features.size(); l++)
                             {
-				if(features.get(l+1).get(docs.get(docID)).containsKey(ent.getKey()))
-                                	line += (l + 1) + ":" + features.get(l+1).get(docs.get(docID)).get(ent.getKey()).getfValue() + " ";
-				else
-					line += (l + 1) + ":0 ";
+                                if(features.get(l+1).get(docs.get(docID)).containsKey(ent.getKey()))
+                                    line += (l + 1) + ":" + features.get(l+1).get(docs.get(docID)).get(ent.getKey()).getfValue() + " ";
+                                else
+                                    line += (l + 1) + ":0 "; 
                             } 
-
                             line += "# " + docID + " " + ent.getKey();
                             if(docs.get(docID).getClasses().contains(ent.getKey()))
                                 line = "1" + line;
                             else
                                 line = "0" + line;
                             bw.write(line + "\n");
+                            all_bw.write(line.replace(" qid:"+ qid, " qid:"+overallQId) + "\n");
                         }
                         qid ++;
+                        overallQId++;
                     }
                 }
-                
                 bw.close();
                 File foldTrain = new File(outDir + "/fold" + (i+1) + "/train.txt");
                 FileWriter fwTrain = new FileWriter(foldTrain);
                 BufferedWriter bwTrain = new BufferedWriter(fwTrain);
                 
-                for(File f : inFiles)
+                for(int j = 0; j < inFiles.length; j++)
                 {
-                    if(!list2.contains(f.getName()))
+                    if(j < i * numFilesPerFold || j >= (i + 1) * numFilesPerFold)
                     {
-                        if(f.getName().startsWith("jrc")&& f.getName().endsWith(".xml")){
-                            String docID = f.getName().split("\\.")[0];
+                        if(inFiles[j].getName().startsWith("jrc")&& inFiles[j].getName().endsWith(".xml")){
+                            String docID = inFiles[j].getName().split("\\.")[0];
                             Map<String, Feature> firstFeature = features.get(1).get(docs.get(docID));
                             for(Entry<String, Feature> ent : firstFeature.entrySet())
                             {
@@ -105,10 +100,10 @@ public class K_Fold_CrossValidation {
                                 
                                 for(int l = 1; l < features.size(); l++)
                                 {
-					if(features.get(l+1).get(docs.get(docID)).containsKey(ent.getKey()))
-                                    		line += (l + 1) + ":" + features.get(l+1).get(docs.get(docID)).get(ent.getKey()).getfValue() + " ";
-					else 
-						line += (l + 1) + ":0 ";
+                                    if(features.get(l+1).get(docs.get(docID)).containsKey(ent.getKey()))
+                                        line += (l + 1) + ":" + features.get(l+1).get(docs.get(docID)).get(ent.getKey()).getfValue() + " ";
+                                    else
+                                        line += (l + 1) + ":0 "; 
                                 }
                                 line += "# " + docID + " " + ent.getKey();
                                 if(docs.get(docID).getClasses().contains(ent.getKey()))
@@ -116,21 +111,19 @@ public class K_Fold_CrossValidation {
                                 else
                                     line = "0" + line;
                                 bwTrain.write(line + "\n");
-                                all_bw.write(line.replace(" qid:"+ qid, " qid:"+overallQId) + "\n");
                             }
                             qid ++;
-                            overallQId++;
                         }
                     }
                 }
                 bwTrain.close();
+                
             }
             all_bw.close();
         } catch (IOException ex) {
             log.error(ex);
         }
     }
-    
     
     public void crossValidation(TreeMap<Integer,HashMap<String,HashMap<String,Feature>>> features, String outDir, String inDir){
         try {
@@ -139,6 +132,9 @@ public class K_Fold_CrossValidation {
             File in = new File(inDir);
             File[] inFiles = in.listFiles();
             Arrays.sort(inFiles);
+            int inNumFiles = inFiles.length;
+            int numFilesPerFold = inNumFiles / k;
+
             File allFold = new File(outDir + "/all_folds.txt" );
             FileWriter all_fw = new FileWriter(allFold);
             BufferedWriter all_bw = new BufferedWriter(all_fw);
@@ -152,47 +148,42 @@ public class K_Fold_CrossValidation {
                 FileWriter fw = new FileWriter(file);
                 BufferedWriter bw = new BufferedWriter(fw);
                 
-                BufferedReader br = new BufferedReader(new FileReader(new File("/home/hazarbo1/data/K_Fold_DOCSNAMES/" + "fold" + i + "/testDocs.txt")));
-                ArrayList<String> list2 = new ArrayList<>();
                 int qid = 0;
-                String s;
-                while((s = br.readLine()) != null)
-                    {
-                        list2.add(s);
-                        {
-                        String docID = s.split("\\.")[0];
+                for(int j = i * numFilesPerFold; j < (i + 1) * numFilesPerFold; j++)
+                {
+                    if(inFiles[j].getName().startsWith("jrc")&& inFiles[j].getName().endsWith(".xml")){
+                        String docID = inFiles[j].getName().split("\\.")[0];
                         Map<String, Feature> firstFeature = features.get(1).get(docID);
                         for(Entry<String, Feature> ent : firstFeature.entrySet())
                         {
                             String line = " " + "qid:" + qid + " " + "1:" + ent.getValue().getfValue() + " ";
-                    
                             for(int l = 1; l < features.size(); l++)
                             {
-				if(features.get(l+1).get(docID).containsKey(ent.getKey()))
-                                	line += (l + 1) + ":" + features.get(l+1).get(docID).get(ent.getKey()).getfValue() + " ";
-				else
-					line += (l + 1) + ":0 ";
+                                if(features.get(l+1).get(docID).containsKey(ent.getKey()))
+                                    line += (l + 1) + ":" + features.get(l+1).get(docID).get(ent.getKey()).getfValue() + " ";
+                                else
+                                    line += (l + 1) + ":0 "; 
                             } 
-
                             line += "# " + docID + " " + ent.getKey();
                             line = ent.getValue().getLabel() + line;
                             bw.write(line + "\n");
+                            all_bw.write(line.replace(" qid:"+ qid, " qid:"+overallQId) + "\n");
                         }
                         qid ++;
+                        overallQId++;
                     }
                 }
-                
                 bw.close();
                 File foldTrain = new File(outDir + "/fold" + (i+1) + "/train.txt");
                 FileWriter fwTrain = new FileWriter(foldTrain);
                 BufferedWriter bwTrain = new BufferedWriter(fwTrain);
                 
-                for(File f : inFiles)
+                for(int j = 0; j < inFiles.length; j++)
                 {
-                    if(!list2.contains(f.getName()))
+                    if(j < i * numFilesPerFold || j >= (i + 1) * numFilesPerFold)
                     {
-                        if(f.getName().startsWith("jrc")&& f.getName().endsWith(".xml")){
-                            String docID = f.getName().split("\\.")[0];
+                        if(inFiles[j].getName().startsWith("jrc")&& inFiles[j].getName().endsWith(".xml")){
+                            String docID = inFiles[j].getName().split("\\.")[0];
                             Map<String, Feature> firstFeature = features.get(1).get(docID);
                             for(Entry<String, Feature> ent : firstFeature.entrySet())
                             {
@@ -200,25 +191,24 @@ public class K_Fold_CrossValidation {
                                 
                                 for(int l = 1; l < features.size(); l++)
                                 {
-					if(features.get(l+1).get(docID).containsKey(ent.getKey()))
-                                    		line += (l + 1) + ":" + features.get(l+1).get(docID).get(ent.getKey()).getfValue() + " ";
-					else 
-						line += (l + 1) + ":0 ";
+                                    if(features.get(l+1).get(docID).containsKey(ent.getKey()))
+                                        line += (l + 1) + ":" + features.get(l+1).get(docID).get(ent.getKey()).getfValue() + " ";
+                                    else
+                                        line += (l + 1) + ":0 "; 
                                 }
                                 line += "# " + docID + " " + ent.getKey();
                                 line = ent.getValue().getLabel() + line;
                                 bwTrain.write(line + "\n");
-                                all_bw.write(line.replace(" qid:"+ qid, " qid:"+overallQId) + "\n");
                             }
                             qid ++;
-                            overallQId++;
                         }
                     }
                 }
                 bwTrain.close();
+                
             }
             all_bw.close();
-     } catch (IOException ex) {
+        } catch (IOException ex) {
             log.error(ex);
         }
     }
