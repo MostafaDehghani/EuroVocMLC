27a9a1cf8aeb2b428646732c071bde0e22ad5284
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2014-09-11 12:07:55 +0200

    Merge branch 'master' of https://github.com/MostafaDehghani/EuroVocMLC
    Conflicts:
    	nbactions.xml

--- src/main/java/nl/uva/mlc/settings/Config.java
+++ src/main/java/nl/uva/mlc/settings/Config.java
@@ -1,6 +1,5 @@
 package nl.uva.mlc.settings;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -10,14 +9,13 @@ import java.util.Properties;
  * @author  Mostafa Dehghani
  */
 public class Config {
+    static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(Config.class.getName());
     public static Properties configFile = new Properties();
     static{
         try {
-            	ClassLoader loader = Thread.currentThread().getContextClassLoader();  
-//	        cFile = new File("Config.properties");
-//              InputStream stream = new FileInputStream(cFile);            
 	      InputStream stream = Config.class.getResourceAsStream("/Config.properties");
-          configFile.load(stream);  
+              log.info("Confog file path: " + Config.class.getResource("/Config.properties").getPath());
+              configFile.load(stream);  
         } catch (IOException e) {
             e.printStackTrace();
         }
